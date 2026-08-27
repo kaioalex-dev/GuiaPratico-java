03aa60d44c5dd53a49f6b94ce63ed6f1b9aef9a9
DADOS-06 <dados-06@MacBook-Pro-de-ELDORADO.local>
2026-08-27 16:50:07 -0300

    first commit

---
+++ byte 8 bits byte x = 10; Inteiro pequeno.java
@@ -0,0 +1,8 @@
+byte	8 bits	byte x = 10;	Inteiro pequeno
+short	16 bits	short x = 1000;	Inteiro
+int	32 bits	int x = 100000;	Inteiro, o mais usado
+long	64 bits	long x = 100000L;	Inteiro grande
+float	32 bits	float x = 3.14f;	Número decimal
+double	64 bits	double x = 3.14;	Decimal, maior precisão
+char	16 bits	char x = 'A';	Um caractere Unicode
+boolean	JVM-dependente*	boolean x = true;	true ou false
