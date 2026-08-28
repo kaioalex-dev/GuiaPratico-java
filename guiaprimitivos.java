d7d60137430d8ce49ae2ac2ce9d253b2814e6fdd
luansilva2104 <luan.nascimentodasilva2104@gmail.com>
2026-08-27 23:59:30 -0300

    Add MJ biography variables in guiaprimitivos.java
    Added variables related to Michael Jackson's biography.

--- guiaprimitivos.java
+++ guiaprimitivos.java
@@ -1,8 +1,9 @@
-byte	8 bits	byte x = 10;	Inteiro pequeno
-short	16 bits	short x = 1000;	Inteiro
-int	32 bits	int x = 100000;	Inteiro, o mais usado
-long	64 bits	long x = 100000L;	Inteiro grande
-float	32 bits	float x = 3.14f;	Número decimal
-double	64 bits	double x = 3.14;	Decimal, maior precisão
-char	16 bits	char x = 'A';	Um caractere Unicode
-boolean	JVM-dependente*	boolean x = true;	true ou false
+byte idade = 13 //Idade que mj comeuçou a cerreira solo
+short ano = 1958 //Ano que nasceu
+int vendas = 120000000 //Vendas do disco de Thriller
+long visualizacoes = 1000000000 //Números de visualizações
+float altura = 1.75f //Altura do mj
+double patrimonio = 570000000 //Patrimônio do mj
+char letra = "m" //Inicial do mj
+boolean cantor = true //Se é cantor ou não
+  
