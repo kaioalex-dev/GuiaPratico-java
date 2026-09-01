e49a8886bdf8ddabfad7ce5634735b7e1d31e5eb
luansilva2104 <luan.nascimentodasilva2104@gmail.com>
2026-09-01 16:31:49 -0300

    Delete guiaprimitivos.java
    deletando arquivo para organização do repositório.

--- guiaprimitivos.java
+++
@@ -1,9 +0,0 @@
-byte idade = 13 //Idade que mj comeuçou a cerreira solo
-short ano = 1958 //Ano que nasceu
-int vendas = 120000000 //Vendas do disco de Thriller
-long visualizacoes = 1000000000 //Números de visualizações
-float altura = 1.75f //Altura do mj
-double patrimonio = 570000000 //Patrimônio do mj
-char letra = "m" //Inicial do mj
-boolean cantor = true //Se é cantor ou não
-  
